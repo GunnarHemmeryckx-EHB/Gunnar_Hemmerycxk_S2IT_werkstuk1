ed5f0a59927bd557245e2acc3b9bc3403bdb7afb
Gunnar Hemmeryckx <gunnar.hemmeryckx@student.ehb.be>
2018-03-31 00:04:22 +0200

    2e tab verder uitgewerkt

--- Werkstuk1/SecondTabViewController.swift
+++ Werkstuk1/SecondTabViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondTabViewController.swift
 //  Werkstuk1
 //
 //  Created by Gunnar Hemmeryckx on 30/03/18.
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class SecondTabViewController: UIViewController,MKMapViewDelegate {
+   
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var persoon1 = Persoon(naam: "Kenobi", voornaam: "Obi-wan", afbeelding: "ObiWanKenobi", straat: "Jundland Wastes", huisnummer: 3, postcode: 1374, gemeente: "Tatooine", telefoonnummer: "0455671023", coordLat: 33.740833, coordLong: 10.734951)
+    var persoon2 = Persoon(naam: "Jinn", voornaam: "Qui-Gon", afbeelding: "QuiGon_Jinn", straat: "Form IV", huisnummer: 12, postcode: 1734, gemeente: "Ataru", telefoonnummer: "0434211697", coordLat: 41.073485, coordLong: 14.327004)
+    var persoon3 = Persoon(naam: "Boba", voornaam: "Fett", afbeelding: "BobaFett", straat: "Great Pit of Carkoon", huisnummer: 1, postcode: 1373, gemeente: "Tatooine", telefoonnummer: "0454102201", coordLat: 32.377246, coordLong: -113.891700)
+    var persoon4 = Persoon(naam: "(Chewie)", voornaam: "Chewbacca", afbeelding: "Chewbacca", straat: "Forest", huisnummer: 6, postcode: 1823, gemeente: "Kashyyyk", telefoonnummer: "0446123434", coordLat: 8.247011, coordLong: 98.563542)
+    
+    var persoonArray = [Persoon]()
+    
+    
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        persoonArray.append(persoon1)
+        persoonArray.append(persoon2)
+        persoonArray.append(persoon3)
+        persoonArray.append(persoon4)
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        for persoon in persoonArray {
+            let annotation = MyAnnotation(coordinate: CLLocationCoordinate2D(latitude: persoon.coordLat, longitude: persoon.coordLong), title: persoon.voornaam + " " + persoon.naam)
+            self.mapView.addAnnotation(annotation)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
--- Werkstuk1/TableViewController.swift
+++ Werkstuk1/TableViewController.swift
@@ -9,46 +9,50 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
     var persoon1 = Persoon(naam: "Kenobi", voornaam: "Obi-wan", afbeelding: "ObiWanKenobi", straat: "Jundland Wastes", huisnummer: 3, postcode: 1374, gemeente: "Tatooine", telefoonnummer: "0455671023", coordLat: 33.740833, coordLong: 10.734951)
     var persoon2 = Persoon(naam: "Jinn", voornaam: "Qui-Gon", afbeelding: "QuiGon_Jinn", straat: "Form IV", huisnummer: 12, postcode: 1734, gemeente: "Ataru", telefoonnummer: "0434211697", coordLat: 41.073485, coordLong: 14.327004)
+    var persoon3 = Persoon(naam: "Boba", voornaam: "Fett", afbeelding: "BobaFett", straat: "Great Pit of Carkoon", huisnummer: 1, postcode: 1373, gemeente: "Tatooine", telefoonnummer: "0454102201", coordLat: 32.377246, coordLong: -113.891700)
+    var persoon4 = Persoon(naam: "(Chewie)", voornaam: "Chewbacca", afbeelding: "Chewbacca", straat: "Forest", huisnummer: 6, postcode: 1823, gemeente: "Kashyyyk", telefoonnummer: "0446123434", coordLat: 8.247011, coordLong: 98.563542)
     
     //ARRAY AANMAKEN
     var persoonArray = [Persoon]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         persoonArray.append(persoon1)
         persoonArray.append(persoon2)
+        persoonArray.append(persoon3)
+        persoonArray.append(persoon4)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return persoonArray.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "persoonCell", for: indexPath)
-
+        
         // Configure the cell...
         cell.imageView?.image = UIImage(named: persoonArray[indexPath.row].afbeelding)//CELL IMAGE INSTELLEN -> GAAT ZOEKEN NAAR EEN IMAGE IN ASSETS MET MEEGEGEVEN NAAM
         cell.textLabel?.text = persoonArray[indexPath.row].voornaam + " " + persoonArray[indexPath.row].naam//CELL TITEL = TITEL VAN ITEM
@@ -56,45 +60,45 @@ class TableViewController: UITableViewController {
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -105,5 +109,5 @@ class TableViewController: UITableViewController {
         }
     }
     
-
+    
 }
